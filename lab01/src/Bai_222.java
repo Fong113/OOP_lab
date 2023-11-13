e7b37cb2211ed9f3e418ba18171a53965de66006
phongdv-0421 <fongcypher@gmail.com>
2023-11-13 22:09:26 +0700

    Add a feature for demonstration

--- lab01/src/Bai_222.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-public class Bai_222 {
-    public static void main(String[] args) {
-                JOptionPane.showMessageDialog(null, "Do Van Phong-5111-Hello world! How are you ?");
-                System.exit(0);
-    }
-
-}
